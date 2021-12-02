f9ca88d790ec8dde86f05c7b7fba49776129a933
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2021-12-02 16:21:30 -0500

    npm module packages
    install validation package from npm
    and chalk package.

--- notes-app/app.js
+++ notes-app/app.js
@@ -2,35 +2,55 @@
 // // And the name of the variable doesn't need to match the name of the variable on the utils.js file
 // const add = require('./utils.js');
 // // That's for the second Challenge
-const getNotes = require('./notes.js');
+// const getNotes = require('./notes.js');
 // // we create a variable to import fs(file system) documentations
 // const fs = require('fs');
+// // require the validation packages
+// const validator = require('validator');
+// // Challenge 3 require chalk
+const chalk = require('chalk');
 // // ----------------------------------------------------------------------------- // //
 // // ============= Practice ================= // //
 // const sum = add(4, 5);
 
 // console.log(sum);
 
-// //   ========   Challenge 1  ========   //
+// // imptort URL validations from the validator package
+// console.log(validator.isURL('http://github.com'));
 
+// //   ========   Challenge 1  ========   //
 // //-- Append a message to notes.txt --//
+
 // // 1. Use appendFileSync to append to the file
 // // 2. Run the script with
 // // 3. Check your work by opening the file and viewing the appended text
 
 // // Create a txt file, and add a note(what ever we pass in the second quetations) to the txt file.
 // fs.writeFileSync('notes.txt', 'My Name is Georgios.');
+
 // // Append/add more notes in the excisting file.
 // fs.appendFileSync('notes.txt', " But i'm no longer live there, i moved in the U.S in 2011.");
 
 // //   ========   Challenge 2  ========   //
-
 // //-- Define and use a function in a new file --//
+
 // // 1. Create a new file called notes.js
 // // 2. Create getNotes funtion that returns "Your notes..."
 // // 3. Export getNotes function
-// // 3. From app.js, load in and call function printing message to console
+// // 4. From app.js, load in and call function printing message to console
 
-const msg = getNotes();
+// const msg = getNotes();
 
-console.log(msg);
+// console.log(msg);
+
+// //   ========   Challenge 3  ========   //
+// //-- Use the chalk library in your project --//
+
+// // 1. Install version 5.0.0 of chalk
+// // 2. Load chalk into app.js
+// // 3. Use it to print the string "Success!" to the console in green
+// // 4. Test your work
+// //
+// // Bonus: Use docs to mess with other styles. Make text bold and inversed.
+
+console.log(chalk.green('Success!'));
