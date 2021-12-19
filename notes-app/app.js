aea1d94e155bc95c370d1823581f6027284e3969
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2021-12-18 19:49:54 -0500

    Update
    clean comments

--- notes-app/app.js
+++ notes-app/app.js
@@ -1,5 +1,5 @@
-// // we need to creat a variable that requires to load the name from the utils.js file
-// // And the name of the variable doesn't need to match the name of the variable on the utils.js file
+/* We need to creat a variable that requires to load the name from the utils.js file
+And the name of the variable doesn't need to match the name of the variable on the utils.js file */
 // const add = require('./utils.js');
 // // That's for the second Challenge
 const notes = require('./notes.js');
@@ -30,49 +30,11 @@ const yargs = require('yargs');
 // 	console.log('Removing note!');
 // }
 
-// //   ========   Challenge 1  ========   //
-// //-- Append a message to notes.txt --//
-
-// // 1. Use appendFileSync to append to the file
-// // 2. Run the script with
-// // 3. Check your work by opening the file and viewing the appended text
-
 // // Create a txt file, and add a note(what ever we pass in the second quetations) to the txt file.
 // fs.writeFileSync('notes.txt', 'My Name is Georgios.');
 
 // // Append/add more notes in the excisting file.
 // fs.appendFileSync('notes.txt', " But i'm no longer live there, i moved in the U.S in 2011.");
-
-// //   ========   Challenge 2  ========   //
-// //-- Define and use a function in a new file --//
-
-// // 1. Create a new file called notes.js
-// // 2. Create getNotes funtion that returns "Your notes..."
-// // 3. Export getNotes function
-// // 4. From app.js, load in and call function printing message to console
-
-// const msg = getNotes();
-
-// console.log(msg);
-
-// //   ========   Challenge 3  ========   //
-// //-- Use the chalk library in your project --//
-
-// // 1. Install version 5.0.0 of chalk
-// // 2. Load chalk into app.js
-// // 3. Use it to print the string "Success!" to the console in green
-// // 4. Test your work
-// //
-// // Bonus: Use docs to mess with other styles. Make text bold and inversed.
-
-// console.log(chalk.inverse.green.bold('Success!'));
-
-// //   ========   Challenge 4  ========   //
-// //-- Add two new commands --//
-
-// // 1. Setup command to support "list" command (print placeholder message for now)
-// // 2. Setup command to support "read" command (print placeholder message for now)
-// // 3. Test your work by running both commands and ensure correct output
 
 // //--------- Customize yargs version --------// //
 yargs.version('1.1.0');
