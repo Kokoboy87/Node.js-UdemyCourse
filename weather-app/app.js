cf97aff8b96d67da92bd7880415f8c88b1d35829
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2021-12-24 03:50:30 -0500

    Update app.js
    Fetch data from a weather API

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,12 +1,14 @@
-/*
-console.log('Starting');
+const request = require('request');
 
-setTimeout(() => {
-	console.log('2 Second Timer');
-}, 2000);
+const url = 'http://api.weatherstack.com/current?access_key=ff8269c5b0bd46ba6e40cce113c0e3e8&query=37.8267,-122.4233&units=f';
 
-setTimeout(() => {
-	console.log('0 second Timer');
-}, 0);
-console.log('Stopping');
-*/
+request({ url: url, json: true }, (error, response) => {
+	const temp = response.body.current.temperature;
+	const feels = response.body.current.feelslike;
+	const description = response.body.current.weather_descriptions[0];
+	console.log(`${description}. It is currently ${temp} degreess out. It feels like ${feels} degreess out.`);
+	/*Another way of doing this
+	const current = response.body.current;
+	console.log(`it is currently ${current.temperature} degreess out. It feels like ${current.feelslike} degreess out.`);
+	*/
+});
