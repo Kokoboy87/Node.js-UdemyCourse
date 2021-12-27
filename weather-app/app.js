7390585c53e1f9e4f3b13c579177f1590d5dc9ce
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2021-12-27 03:05:26 -0500

    Update app.js

--- weather-app/app.js
+++ weather-app/app.js
@@ -7,15 +7,9 @@ request({ url: url, json: true }, (error, response) => {
 	console.log(`${current.weather_descriptions[0]}. It is currently ${current.temperature} degreess out. It feels like ${current.feelslike} degreess out.`);
 });
 
-const urlGeo = 'https://api.mapbox.com/geocoding/v5/mapbox.places/alexandria,virginia.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoia29rb2JveSIsImEiOiJja3hvYWtqZGQ3cGxlMnVybmlvMzMybWg5In0.8FjBCn9yDSwNjT2RTHewOg&limit=1';
+const geoURL = 'https://api.mapbox.com/geocoding/v5/mapbox.places/alexandria,virginia.json?proximity=-74.70850,40.78375&access_token=pk.eyJ1Ijoia29rb2JveSIsImEiOiJja3hvYWtqZGQ3cGxlMnVybmlvMzMybWg5In0.8FjBCn9yDSwNjT2RTHewOg&limit=1';
 
-request({ url: urlGeo, json: true }, (error, response) => {
+request({ url: geoURL, json: true }, (error, response) => {
 	const location = response.body;
 	console.log(`The longtitude is ${location.features[0].center[0]}, and the latitude is ${location.features[0].center[1]}`);
 });
-// Goal : Print the lat/lon for Los Angeles
-//
-//1. Fire off a new request module to URL explored in browser
-//2. Have the request module parse it as JSON
-//3. Print both the latitude and longtitude to the terminal
-//4. Test your work!
