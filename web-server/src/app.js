28cca4a04d56a3ce962d1f68626470ee643c8973
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2022-01-09 16:52:21 -0500

    Weather App update
    Customizing the Views Directory. Advance Templating. 404 Page.

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,18 +1,24 @@
-// load and store the path library
+// Load the libraries
 const path = require('path');
-//variable to load the library Express
 const express = require('express');
-
-//console.log(__dirname);
-//console.log(path.join(__dirname, '../public'));
+const hbs = require('hbs');
 
 // Store the express application
 const app = express();
-// A static way to navigate to the html page route
+
+// Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public');
-// Method to set a value and get handlebars set up.
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+// Setup handlebars engine and views location
 app.set('view engine', 'hbs');
+app.set('views', viewsPath);
+hbs.registerPartials(partialsPath);
+
+// Setup a static directory to serve
 app.use(express.static(publicDirectoryPath));
+
 // Method to navigate to the home page route
 app.get('', (req, res) => {
 	// rendering dynamic content
@@ -28,19 +34,35 @@ app.get('/about', (req, res) => {
 		name: 'Georgios Kokotinis',
 	});
 });
-
+// Method to navigate to the help page route
 app.get('/help', (req, res) => {
 	res.render('help', {
-		title: 'You get helped by',
+		titleText: 'This is some helpfull text',
+		title: 'Help',
 		name: 'Georgios Kokotinis',
 	});
 });
-
 // Method to navigate to the weather page route
 app.get('/weather', (req, res) => {
 	res.send({
 		forecast: 'Rainy weather',
 		location: 'Landmark, VA',
+	});
+});
+
+app.get('/help/*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		name: 'Georgios Kokotinis',
+		errorMessage: 'Help article not found',
+	});
+});
+
+app.get('*', (req, res) => {
+	res.render('404', {
+		title: '404',
+		name: 'Georgios Kokotinis',
+		errorMessage: 'Page not found',
 	});
 });
 
