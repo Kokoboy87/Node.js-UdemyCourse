20b0f0e42ffea753a47f8a7c98095c77ab0ac56a
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2022-01-26 18:57:56 -0500

    Task App Update
    Querying Documents, Promises

---
+++ playground/8-promises.js
@@ -0,0 +1,25 @@
+const doWorkPromise = new Promise((resolve, reject) => {
+	setTimeout(() => {
+		//resolve([2, 7, 1]);
+		reject('Things went wrong!');
+	}, 2000);
+});
+
+doWorkPromise
+	.then((result) => {
+		console.log('Success!', result);
+	})
+	.catch((error) => {
+		console.log('Error!', error);
+	});
+
+//
+//
+//                              fulfilled
+//                            /
+// Promise      -- pending -->
+//                            \
+//                              rejected
+//
+//
+//
--- task-manager/mongodb.js
+++ task-manager/mongodb.js
@@ -1,10 +1,17 @@
 // CRUD create read update delete
 
-const mongodb = require('mongodb');
-const MongoClient = mongodb.MongoClient;
+//const mongodb = require('mongodb');
+//const MongoClient = mongodb.MongoClient;
+//const ObjectID = mongodb.ObjectID;
+
+const { MongoClient, ObjectId } = require('mongodb');
 
 const connectionURL = 'mongodb://127.0.0.1:27017';
 const databaseName = 'task-manager';
+
+//const id = new ObjectId();
+//console.log(id);
+//console.log(id.toHexString().length);
 
 MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) => {
 	if (error) {
@@ -13,60 +20,34 @@ MongoClient.connect(connectionURL, { useNewUrlParser: true }, (error, client) =>
 
 	const db = client.db(databaseName);
 
-	// --------- That's the first of the CRUD operations 'Create'. --------- //
-	//	db.collection('users').insertOne(
-	//		{
-	//			name: 'Georgios',
-	//			age: 34,
-	//		},
-	//		(error, result) => {
-	//			if (error) {
-	//				return console.log('Unable to insert user');
-	//			}
-	//			console.log(result.insertedId);
-	//		}
-	//	);
-	//db.collection('users').insertMany(
-	//	[
-	//		{
-	//			name: 'Maria',
-	//			age: 22,
-	//		},
-	//		{
-	//			name: 'Antreas',
-	//			age: 45,
-	//		},
-	//	],
-	//	(error, result) => {
-	//		if (error) {
-	//			return console.log('Unable to insert documents!');
-	//		}
-
-	//		console.log(result.insertedIds);
+	//db.collection('users').findOne({ _id: new ObjectId('61ef9f18bb9154009e4d6fb3') }, (error, user) => {
+	//	if (error) {
+	//		return console.log('Unable to fetch user');
 	//	}
-	//);
 
-	// --------- That's the first of the CRUD operations 'Create'. --------- //
-	db.collection('tasks').insertMany(
-		[
-			{
-				description: 'Grocery shopping',
-				completed: false,
-			},
-			{
-				description: 'Give a bath to my dog',
-				completed: false,
-			},
-			{
-				description: 'Apply for jobs',
-				completed: true,
-			},
-		],
-		(error, result) => {
-			if (error) {
-				return console.log('Unable to insert documents!');
-			}
-			console.log(result.insertedIds);
+	//	console.log(user);
+	//});
+
+	//db.collection('users')
+	//	.find({ age: 34 })
+	//	.toArray((error, users) => {
+	//		console.log(users);
+	//	});
+	db.collection('tasks').findOne({ _id: new ObjectId('61ef369e5427e1d6da3d455c') }, (error, task) => {
+		if (error) {
+			return console.log('Unable to fetch tasks');
 		}
-	);
+
+		console.log(task);
+	});
+
+	db.collection('tasks')
+		.find({ completed: false })
+		.toArray((error, tasks) => {
+			if (error) {
+				return console.log('Unable to fetch tasks');
+			}
+
+			console.log(tasks);
+		});
 });
