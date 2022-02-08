1664afb74d0504efe722a5fa438eb115748a7a0c
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2022-02-08 15:36:10 -0500

    Task App Update
    Setup the REST API route for Create

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,46 +1,6 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api');
-
-const User = mongoose.model('User', {
-	name: {
-		type: String,
-		required: true,
-		trim: true,
-	},
-	email: {
-		type: String,
-		required: true,
-		trim: true,
-		lowercase: true,
-		validate(value) {
-			if (!validator.isEmail(value)) {
-				throw new Error('Email is not valid');
-			}
-		},
-	},
-	password: {
-		type: String,
-		required: true,
-		trim: true,
-		minLength: 7,
-		validate(value) {
-			if (value.toLowerCase().includes('password')) {
-				throw new Error("Password can't be 'password'");
-			}
-		},
-	},
-	age: {
-		type: Number,
-		default: 0,
-		validate(value) {
-			if (value < 0) {
-				throw new Error('Age must be a positive number');
-			}
-		},
-	},
-});
 
 //const me = new User({
 //	name: '  Georgios  ',
@@ -67,16 +27,3 @@ const Tasks = mongoose.model('Tasks', {
 		default: false,
 	},
 });
-
-const task = new Tasks({
-	description: 'I Have to cook   ',
-	//completed: true,
-});
-
-task.save()
-	.then(() => {
-		console.log(task);
-	})
-	.catch((error) => {
-		console.log('Error!', error);
-	});
--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -1,10 +1,28 @@
-// returns
+// Loading express
 const express = require('express');
+// require the mongoose
+require('./db/mongoose');
+// Load user in
+const User = require('./models/user');
 // Creating new Express application
 const app = express();
 // Port Value for Heroku deployment
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
+app.post('/users', (req, res) => {
+	const user = new User(req.body);
+
+	user.save()
+		.then(() => {
+			res.send(user);
+		})
+		.catch(e => {
+			res.status(400).send(e);
+		});
+});
+
 app.listen(port, () => {
-	console.log('Server is up on port');
+	console.log(`Server is up on port ${port}`);
 });
