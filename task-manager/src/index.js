8e6015a7f269149981ee15975ba1ed9ea63c3e18
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2022-02-16 20:29:34 -0500

    Task App Updates
    Setup the task creation endpoint

--- task-manager/src/index.js
+++ task-manager/src/index.js
@@ -4,11 +4,13 @@ const express = require('express');
 require('./db/mongoose');
 // require user file to load the user
 const User = require('./models/user');
+// require task file to load the Tasks
+const Task = require('./models/task');
 // Creating new Express application
 const app = express();
 // Port Value for Heroku deployment
 const port = process.env.PORT || 3000;
-// setting up a call to pass income
+// setting up a call function to parse income json to an object so we can access it in our request handlers.
 app.use(express.json());
 
 app.post('/users', (req, res) => {
@@ -16,7 +18,19 @@ app.post('/users', (req, res) => {
 
 	user.save()
 		.then(() => {
-			res.send(user);
+			res.status(201).send(user);
+		})
+		.catch(e => {
+			res.status(400).send(e);
+		});
+});
+
+app.post('/tasks', (req, res) => {
+	const task = new Task(req.body);
+
+	task.save()
+		.then(() => {
+			res.status(201).send(task);
 		})
 		.catch(e => {
 			res.status(400).send(e);
