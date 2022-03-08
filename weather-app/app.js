4db3ef5bcad1a00b0ee006d553ea196fcffd2e0f
Kokoboy87 <75341032+Kokoboy87@users.noreply.github.com>
2022-03-08 13:26:11 -0500

    update(delete comment)

--- weather-app/app.js
+++ weather-app/app.js
@@ -3,12 +3,6 @@ const forecast = require('./utils/forecast');
 
 const address = process.argv[2];
 
-// Goal: Use both destructuring and property shorthand in weather app
-//
-// 1. Use destructuring in app.js, forecast.js, and geocode.js
-// 2. Use property shorhand in forecast.js and geocode.js
-// 3. Test your work and ensure app still works
-
 if (!address) {
 	console.log('Address need to be provided!');
 } else {
